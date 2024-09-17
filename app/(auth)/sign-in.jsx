c1ca2d58847109f14999c7bd051d307951617b43
Aniket Yadav <aniketyadav690@gmail.com>
2024-09-17 14:56:02 +0530

    UI: authscreens

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,14 +1,67 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ScrollView, Text, View, Image } from "react-native";
+import React, { useState } from "react";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import FormField from "../components/FormField";
+import CustomButton from "../components/CustomButton";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  const submit = () => {};
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="h-full bg-primary">
+      <ScrollView>
+        <View className="justify-center w-full min-h-[90vh] px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="mt-10 text-2xl text-white text-semibold font-psemibold">
+            Log in to Aora
+          </Text>
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address" //this is for auto-filling
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
 
-export default SignIn
+          <CustomButton
+            title={"Sign In"}
+            containerStyles={"mt-7"}
+            isLoading={isSubmitting}
+            handleSubmit={submit}
+          />
+          <View className="flex flex-row justify-center gap-2 pt-5">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have an account?
+            </Text>
+            <Link
+              href="/sign-up"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Signup
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default SignIn;
--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,14 +1,75 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { ScrollView, Text, View, Image } from "react-native";
+import React, { useState } from "react";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import FormField from "../components/FormField";
+import CustomButton from "../components/CustomButton";
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  const submit = () => {};
+
   return (
-    <View>
-      <Text> SignUP</Text>
-    </View>
+    <SafeAreaView className="h-full bg-primary">
+      <ScrollView>
+        <View className="justify-center w-full min-h-[90vh] px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="mt-10 text-2xl text-white text-semibold font-psemibold">
+            Sign up to Aora
+          </Text>
+          <FormField
+            title="Username"
+            value={form.username}
+            handleChangeText={(e) => setForm({ ...form, username: e })}
+            otherStyles="mt-7"
+          />
+
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address" //this is for auto-filling
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+
+          <CustomButton
+            title={"Sign Up"}
+            containerStyles={"mt-7"}
+            isLoading={isSubmitting}
+            handleSubmit={submit}
+          />
+          <View className="flex flex-row justify-center gap-2 pt-5">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Already having an account?
+            </Text>
+            <Link
+              href="/sign-in"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Login
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 export default SignUp;
-
-const styles = StyleSheet.create({});
